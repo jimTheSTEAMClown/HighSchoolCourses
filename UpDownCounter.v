5e09a1d4f68a7577f1921a13199d0d1f6484f832
Jim Burnham <jim.the.steam.clown@gmail.com>
2023-06-05 18:14:20 -0700

    Update UpDownCounter.v
    Signed-off-by: Jim Burnham <jim.the.steam.clown@gmail.com>

--- UpDownCounter.v
+++ UpDownCounter.v
@@ -3,7 +3,7 @@
 //When Down mode is selected, counter counts from 15 to 0 and then again from 15 to 0.
 //Changing mode doesn't reset the Count value to zero.
 //You have apply high value to reset, to reset the Counter output.
-module up-down-counter(
+module updowncounter(
     Clk,
     reset,
     UpDown,  //high for UP counter and low for Down counter
