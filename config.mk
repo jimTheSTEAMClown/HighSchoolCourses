6cc28e107d4a8a0ac8db8a506c05959bd6db4583
Jim Burnham <jim.the.steam.clown@gmail.com>
2023-07-13 17:22:46 -0700

    Create config.mk
    Signed-off-by: Jim Burnham <jim.the.steam.clown@gmail.com>

---
+++ config.mk
@@ -0,0 +1,10 @@
+export DESIGN_NAME = UpDownCounter
+export PLATFORM    = nangate45
+
+export VERILOG_FILES = ./designs/src/$(DESIGN_NAME)/UpDownCounter.v
+export SDC_FILE      = ./designs/$(PLATFORM)/$(DESIGN_NAME)/constraint.sdc
+export ABC_AREA      = 1
+
+export CORE_UTILIZATION ?= 55  # What should these be? what is the driving contraint?
+export PLACE_DENSITY_LB_ADDON = 0.20
+export TNS_END_PERCENT ?= 100
